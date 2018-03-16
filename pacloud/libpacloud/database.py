92de10cb183c10da5862d77b82a733b774ff2ae7
Varpie <pierre.varlez@gmail.com>
2018-03-16 23:55:07 +0000

    Generalizing package directory and metadata locations

--- pacloud/libpacloud/database.py
+++ pacloud/libpacloud/database.py
@@ -3,33 +3,35 @@
 import os
 import json
 
-DB_DIRECTORY = '/var/lib/pacloud/db/'
+DB_DIR = '/var/lib/pacloud/db/'
+PACKAGE_DIR = lambda pkg_name: '{}{}'.format(DB_DIR, pkg_name)
+METADATA_FILE = lambda pkg_name: '{}/metadata.json'.format(PACKAGE_DIR(pkg_name))
 
 def list_packages():
-    packages = os.listdir(DB_DIRECTORY)
+    packages = os.listdir(DB_DIR)
     return packages
 
 def info_package(package_name):
-    metadata_file = open('{}{}/metadata.json'.format(DB_DIRECTORY, package_name), 'r')
+    metadata_file = open(METADATA_FILE(package_name), 'r')
     data = json.load(metadata_file)
     metadata_file.close()
     return data
 
-# Just a helper function to rewrite a packae metadata, not to be called by other modules.
+# Just a helper function to rewrite a package metadata, not to be called by other modules.
 def rewrite_metadata(package_name, metadata):
-    metadata_file = open('{}/metadata.json'.format(DB_DIRECTORY, package_name), 'w+')
+    metadata_file = open(METADATA_FILE(package_name), 'w+')
     metadata_file.write(json.dumps(metadata))
     metadata_file.close()
 
 def add_package(package_name, metadata):
-    os.makedirs('{}{}'.format(DB_DIRECTORY, package_name))
+    os.makedirs(PACKAGE_DIR(package_name))
     rewrite_metadata(package_name, metadata)
 
 def remove_package(package_name):
-    files = os.listdir('{}{}'.format(DB_DIRECTORY, package_name))
+    files = os.listdir(PACKAGE_DIR(package_name))
     for file in files:
-        os.remove('{}{}/{}'.format(DB_DIRECTORY, package_name, file))
-    os.removedirs('{}{}'.format(DB_DIRECTORY, package_name))
+        os.remove('{}/{}'.format(PACKAGE_DIR(package_name), file))
+    os.removedirs(PACKAGE_DIR(package_name))
 
 def modify_package(package_name, new_metadata):
     current_metadata = info_package(package_name)
