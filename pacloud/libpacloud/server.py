975915460e83f3c093aefaa3adbb4373866604be
Varpie <pierre.varlez@gmail.com>
2018-03-20 23:57:34 +0000

    Adding simple package retrieving functions
    Used with a simple REST API made with NodeJS, SERVER_URL/packages/package_name-version returns an URL to the package, that is then downloaded and placed in the right folder in the DB_DIR.

---
+++ pacloud/libpacloud/server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import urllib.request
+
+from config import SERVER_URL, DB_DIR
+
+def get_package_url(package_name, version):
+    return urllib.request.urlopen('{}/packages/{}-{}'.format(SERVER_URL, package_name, version)).read().decode("utf-8")
+
+def download_package(package_name, version):
+    url = get_package_url(package_name, version)
+    urllib.request.urlretrieve(url, '{}/{}/{}-{}.tar.gz'.format(DB_DIR, package_name, package_name, version))
+
+if __name__ == "__main__":
+    download_package("example", "2.4")
+
