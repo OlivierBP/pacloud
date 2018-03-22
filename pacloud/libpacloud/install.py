1b37709deadbf324b6fe7384ffebf9447eba48c1
Juliett7 <35964948+Juliett7@users.noreply.github.com>
2018-03-22 18:35:16 +0000

    Update install.py
    creation of the tree

--- pacloud/libpacloud/install.py
+++ pacloud/libpacloud/install.py
@@ -1,19 +1,40 @@
 #!/bin/python3
 
-import server
+from server import pkg
+from database import list_packages
+import urllib.request
+import os
+import zipfile
 
-list=[]
 
+list = []
+listdb = list_packages()
+
+#check dependencies
 def check_dep(pkg):
-    if len(pkg.dep == 0) and pkg not in list:
+    if len(pkg.dep == 0) and pkg not in listdb:
         list.append(pkg)
     else:
         for dep in pkg.dep:
-            if pkg not in list:
+            if pkg not in listdb:
                 list.appen(pkg)
-            if dep not in list:
+            if dep not in listdb:
                 check.dep(dep)
+    return list
 
+
+
+def install(package_name, version):
+    zip_ref = zipfile.ZipFile('"{}/{}/{}-{}.tbz2".format(DB_DIR, package_name, package_name, version)','r')
+    zip = zip_ref.extractall('./tmp')
+    zip_ref.close()
+    
+    path = os.abspath(zip)
+
+    if not os.path.exists(path):
+        os.makedirs(exist_ok = True)
+
+    return True
 
 
 if _name_ == '_main_':
