f5d7efc404a39deacd64cc6d1b3a4d414c6ff7c0
Varpie <pierre.varlez@gmail.com>
2018-03-23 10:33:34 +0000

    Installing specific version

--- pacloud/libpacloud/install.py
+++ pacloud/libpacloud/install.py
@@ -14,14 +14,20 @@ def list_dependencies(package_name, version=None):
     #check dependencies
     def check_dep(package_name):
         package = db.info_package(package_name)
-        if len(package["versions"][0]["dependencies"]) == 0 and package["name"] not in list:
-            list.append(package["name"])
-        else:
-            for dep in package["versions"][0]["dependencies"]:
-                if package["name"] not in list:
-                    list.append(package["name"])
-                if dep not in list:
+        try:
+            if(version != None or package["versions"][0]["number"] != package["installed"]):
+                list.append(package["name"])
+                for dep in package["versions"][0]["dependencies"]:
                     check_dep(dep)
+        except KeyError:
+            if len(package["versions"][0]["dependencies"]) == 0 and package["name"] not in list:
+                list.append(package["name"])
+            else:
+                for dep in package["versions"][0]["dependencies"]:
+                    if package["name"] not in list:
+                        list.append(package["name"])
+                    if dep not in list:
+                        check_dep(dep)
 
     check_dep(package_name)
     return list
@@ -33,7 +39,7 @@ def install(package_name, version=None):
         version = db.info_package(package_name)["versions"][0]["number"]
     package_path = "{}/{}/{}-{}.tbz2".format(DB_DIR, package_name, package_name, version)
     if(not os.path.isfile(package_path)):
-        print("Downloading {}-{}".format(package_name, version))
+        print("Downloading {}-{}...".format(package_name, version), end="")
         download_package(package_name, version)
     tar = tarfile.open(package_path)
     tar.extractall('/tmp/{}'.format(package_name))
@@ -44,6 +50,6 @@ def install(package_name, version=None):
     if not os.path.exists(path):
         os.makedirs(path,exist_ok = True)
 """
-    distutils.dir_util.copy_tree('/tmp/{}'.format(package_name),'/')
+    print(distutils.dir_util.copy_tree('/tmp/{}'.format(package_name),'/'))
     db.mark_as_installed(package_name, version)
 
