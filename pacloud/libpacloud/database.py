e36d16235f6b7e19de12b3b483d1d2060a028f61
Varpie <pierre.varlez@gmail.com>
2018-03-28 14:39:52 +0100

    Forgot the required_by when modifying metadata

--- pacloud/libpacloud/database.py
+++ pacloud/libpacloud/database.py
@@ -48,9 +48,11 @@ def remove_package(package_name):
 
 def modify_package(package_name, new_metadata):
     current_metadata = info_package(package_name)
-    # Updating the database doesn't have to change the state of installed packages
+    # Updating the database doesn't have to change the state of installed packages and required_by
     if('installed' in current_metadata):
         new_metadata['installed'] = current_metadata['installed']
+    if('required_by' in current_metadata):
+        new_metadata['required_by'] = current_metadata['required_by']
     _rewrite_metadata(package_name, new_metadata)
 
 def mark_as_installed(package_name, version):
