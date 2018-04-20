fe0b153cff6f883c82e0c73cf828b2308fad84b2
Varpie <pierre.varlez@gmail.com>
2018-04-20 02:28:55 +0100

    Fixing None version and forcing to install package even if already installed

--- pacloud/libpacloud/install.py
+++ pacloud/libpacloud/install.py
@@ -9,21 +9,23 @@ def list_dependencies(package_name, version=None):
     tested = [] # Stop infinite recursion with co-dependent packages
 
     #check dependencies
-    def check_dep(package_name, version=None):
+    def check_dep(package_name, version=None, force=False):
+        if force:
+            list.append((package_name, db._find_package_version(None, package_name, version)))
         if package_name in tested:
             return
         tested.append(package_name)
         installed_version = db.installed_version(package_name)
         dep_package = db.list_dependencies(package_name, version)
         if version == None and installed_version == None and package_name not in list:
-            list.append((package_name,version))
+            list.append((package_name, db._find_package_version(None, package_name, version)))
         elif (version != None) and (version != installed_version):
             list.append((package_name, version))
         for dep, version in dep_package:
             if dep not in list:
                 check_dep(dep, version)
 
-    check_dep(package_name, version)
+    check_dep(package_name, version, force=True)
     return list
 
 def install(package_name, version=None):
